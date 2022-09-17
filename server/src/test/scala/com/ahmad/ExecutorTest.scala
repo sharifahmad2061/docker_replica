b900e5149e72b70d2919720d45d9985b6b9186f5
Ahmad, Sharif (Agoda) <sharif.ahmad@agoda.com>
2022-09-17 14:19:05 +0700

    completes test

--- server/src/test/scala/com/ahmad/ExecutorTest.scala
+++ server/src/test/scala/com/ahmad/ExecutorTest.scala
@@ -4,13 +4,13 @@ import munit.CatsEffectSuite
 import cats.Id
 
 class ExecutorTest extends CatsEffectSuite {
-  val exec = Executor.instance[Id]
+  val exec = Executor.instance[Either[Throwable, *]]
 
   test("Build Empty Image") {
     val hash = exec.build(Build.empty)
     assertEquals(
-      exec.run(hash).getAll,
-      Map.empty,
+      exec.build(Build.empty).flatMap(exec.run).map(_.getAll),
+      Right(Map.empty),
     )
   }
 }
