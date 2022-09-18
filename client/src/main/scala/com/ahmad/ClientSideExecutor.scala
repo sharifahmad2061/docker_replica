ffb35d862869d455259aa8f99bf532eac485cffd
Ahmad, Sharif (Agoda) <sharif.ahmad@agoda.com>
2022-09-18 10:29:59 +0700

    defined instance logic for client side executor

--- client/src/main/scala/com/ahmad/ClientSideExecutor.scala
+++ client/src/main/scala/com/ahmad/ClientSideExecutor.scala
@@ -1,3 +1,13 @@
 package com.ahmad
 
-object ClientSideExecutor {}
+import java.util.concurrent.Executor
+
+object ClientSideExecutor {
+
+  def instance[F[_]]: Executor[F] =
+    new Executor[F] {
+      def build(build: Build): F[Hash] = ???
+      def run(hash: Hash): F[SystemState] = ???
+    }
+
+}
